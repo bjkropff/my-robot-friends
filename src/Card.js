ead6397a7ec6cfa85a665310251e1936e8e14027
Brian Kropff <brian.kropff@acquia.com>
2022-01-01 14:57:22 -0800

    Add the basic Card file

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+	return (
+		<div>
+			<img alt="robot" src="http://www.robohash.org/test" />
+			<div>
+				<h2>Bubba Beebee</h2>
+				<p>bubba.beebee@gmail.com</p>
+			</div>
+		</div>
+	);
+}
+
+export default Card;
